717174371ef16e5b047c530ae88d2b40d8b97232
jamiukayode <.jamiuayepekayode@gmail.com>
2023-10-12 13:30:10 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello React</h1>
+     <button>click</button>
   </React.StrictMode>
 );
 
